580fbb3588bf9837fef0ab5c8dfcdac9444d0515
Arsh <arshgoyal2005@gmail.com>
2022-01-04 17:22:55 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let body = document.body;
+let toggle_btn = document.getElementById("toggle-btn");
+
+toggle_btn.addEventListener("click", themeSwitch);
+
+function themeSwitch(){
+  body.classList.toggle("body-light");
+}
